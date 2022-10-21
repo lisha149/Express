e5246529fa664302ce09850ee7583b48ba5f5a4e
Palisha Shakya <palishashakya@gmail.com>
2022-10-21 12:19:34 +0545

    ejs template engine-- implemented

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express Assignment" });
+  res.render("index", { name: "Palisha Shakya" });
 });
 
 module.exports = router;
